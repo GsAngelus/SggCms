8c817dffbe8956692bb275d97c38418fe40fa912
Ramon Damien James <damien.ramon@laposte.net>
2015-09-24 11:35:55 +0200

    Create french.php

---
+++ Lang/french.php
@@ -0,0 +1,18 @@
+<?php
+/*********************************************************************************************
+** SGGCMS - PHP CMS(Portal)                                                                 **
+** http://www.SggCMS.Stargate-Galactique.com                                                **
+** Author: Stargate-Galactique-Dev-Corporation                                              **
+** License : http://creativecommons.org/licenses/by-nc-sa/4.0/                              **
+** Copyright SGGDC © 2000 - Open Source Solutions - Any reproduction without permission     **
+*********************************************************************************************/
+
+$LangInfo = array(
+  'name' => "French (Français)",
+  'author' => "SggCms",
+  'website' => "http://www.SggCms.stargate-galactique.com/",
+  'htmllang' => "fr-FR",
+  'countrylang' => "fr_FR",
+  'charset' => "UTF-8"
+);
+?>

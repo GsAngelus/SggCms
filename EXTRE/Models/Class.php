0d131726eb182c9ca1dfbc4d69e5bf61bcef3fae
Ramon Damien James <damien.ramon@laposte.net>
2015-09-24 11:31:22 +0200

    Create Class.php

---
+++ EXTRE/Models/Class.php
@@ -0,0 +1,43 @@
+<?php
+/*********************************************************************************************
+** SGGCMS - PHP CMS(Portal) Extern Class                                                    **
+** Your Site url                                                                            **
+** Author: Your Name/Website Name                                                           **
+** License : License                                                                        **
+** Copyright SiteName © date                                                                **
+*********************************************************************************************/
+
+if (!defined("INDEX_CHECK")) {
+ header('Location: Module/Error/CheckIndex');
+}
+
+class ClassName {
+  /**
+   * [$vars description]
+   * @var [type]
+   */
+  private $vars
+
+  /**
+   * [$vars1 description]
+   * @var [type]
+   */
+  public $vars1
+
+  /**
+   * [__construct description]
+   * @param [type] $value [description]
+   */
+  function __construct($value){
+    # code...
+  }
+
+  /**
+   * [FunctionName description]
+   * @param string $value [description]
+   */
+  public function FunctionName($value='') {
+    # code...
+  }
+}
+?>
